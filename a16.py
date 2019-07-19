799d15d938be0f2060dd17ddf03c7b2954b3fefb
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-19 14:08:32 +0530

    Update a16.py

--- a16.py
+++ a16.py
@@ -1,14 +1,16 @@
-a,b=map(int,input().split())
-li1=list(map(int,input().split()))[:a]
-li2=list(map(int,input().split()))[:b]
-res=[]
-for i in range(len(li1)):
-        for j in range(len(li2)):
-                if(li1[i]==li2[j]):
-                        res1.append(li2[j])
-res1.sort()
-li2.sort()
-if(res1==li2):
-        print("YES")
+nc1=list(map(int,input().split()))
+ac1=list(map(int,input().split()))
+b1=list(map(int,input().split()))
+l1=len(ac1)
+m1=len(b1)
+s1=[]
+for i in range(l1):
+    for j in range(m1):
+        if(ac1[i]==b1[j]):
+            s1.append(b1[j])
+b1.sort()
+s1.sort()
+if(s1==b1):
+    print("YES")
 else:
-        print("NO")
+    print("NO")
