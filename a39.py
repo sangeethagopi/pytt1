f9bc7f0228f590550633cc10e4fa7ddce31d3e17
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-19 14:47:18 +0530

    Update a39.py

--- a39.py
+++ a39.py
@@ -5,7 +5,7 @@ for _ in range(apr):
 for ic in range(len(l1)):
     if('0' in l1[ic]):
         l1[ic]=l1[ic].replace('0','')
-    l1[i]=l1[ic].replace(' ','')
+    l1[ic]=l1[ic].replace(' ','')
 lengths=[]
 for ic in l1:
     if(len(ic)>0):
