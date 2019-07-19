f2959d60ed8246f67a1e48af68316d36561f8d55
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-19 14:16:26 +0530

    Create a20.py

---
+++ a20.py
@@ -0,0 +1,17 @@
+gg1=int(input())
+k=[]
+for i in range (0,gg1):
+	k.append(input())
+l1=len(k[0])
+e1=""
+for i in range (0,l1):
+	c=k[0][i]
+	f=0
+	for j in range (0,gg1):
+		if(c!=k[j][i]):
+			f=1
+	if(f==0):
+		e1=e1+c
+	else:
+		break
+print(e1)
