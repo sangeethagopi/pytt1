a66c670f0988e20f5ccc4fde608e4926372269c8
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-19 14:34:17 +0530

    Rename a32.py to a33.py

---
+++ a33.py
@@ -0,0 +1,12 @@
+sc1,t1 = map(int,input().split())
+l11 = []
+l21 = []
+l11 = input().split()
+for ic in range(len(l11)):
+    l11[ic] = int(l11[ic])
+for ic in range(t1):
+    a,b = map(int,input().split())
+    res = 0
+    for ic in range(a-1,b):
+        res += l11[i]
+    print(res)
